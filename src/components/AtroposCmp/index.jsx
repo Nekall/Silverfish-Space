e1486e688eb3a3c942ec4cd470c04c659bb74f9d
Neka <nekoanchor@yahoo.fr>
2023-05-31 20:18:45 +0000

    feat: update banner

--- src/components/AtroposCmp/index.jsx
+++ src/components/AtroposCmp/index.jsx
@@ -3,7 +3,7 @@ import Atropos from 'atropos/react';
 const AtroposCmp = ({ children }) => {
   return (
     <div id="app">
-      <Atropos className="my-atropos" shadow={false} highlight={false} activeOffset={10}>
+      <Atropos className="my-atropos" shadow={false} highlight={false} >
         {children}
       </Atropos>
     </div>
