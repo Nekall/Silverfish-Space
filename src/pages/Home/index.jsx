0baaab5dd606feea99c9f6897d809562a09e7adf
Neka <nekoanchor@yahoo.fr>
2023-06-10 10:24:07 +0000

    setup: maintenance

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
-// Components
-import Background from "components/Background";
-import Dashboard from "components/Dashboard";
+// Assets
+import { useEffect } from "react";
+import nekamc from "../../assets/videos/neka_mc.webm"
 
-const Home = () => (
+const Home = () => {
+
+  useEffect(() => {
+    document.querySelector('video').playbackRate = 2.0;
+  }, [])
+
+  return (
     <main>
-      <Background>
-        <Dashboard/>
-      </Background>
+      <div className="maintenance">
+        <video width="500" height="400" autoPlay muted playsInline loop>
+          <source src={nekamc} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+        <p>Je manque de redstone,</p>
+        <p> le site arrive bientôt...</p>
+      </div>
     </main>
-  );
-  
+  )
+};
+
 export default Home;
