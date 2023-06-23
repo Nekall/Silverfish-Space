62b2d2b3c54ffd3e8d384adde750ee74bbb2cf72
Neka <nekoanchor@yahoo.fr>
2023-06-23 14:11:18 +0000

    feat: add v2 error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 
 const Error = () => (
-    <div className="__error">
-      <p className="e404">Erreur 404</p>
+  <div className="__error">
+    <div className="__container">
+      <p className="__e404">Erreur 404</p>
       <p>Tu t'es perdu ? </p>
-      <p><NavLink to="/">Retourner sur la page d'accueil</NavLink></p>
+      <p className="__link"><NavLink to="/">Retourner sur la page d'accueil</NavLink></p>
     </div>
-  );
+  </div>
+);
 export default Error;
