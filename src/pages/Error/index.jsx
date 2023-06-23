0bec10076cfccddd9ffc776a0dce41fd0e72f434
Neka <nekoanchor@yahoo.fr>
2023-06-23 14:03:17 +0000

    feat: add v2 achievements page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 
 const Error = () => (
-    <div className="error">
+    <div className="__error">
       <p className="e404">Erreur 404</p>
       <p>Tu t'es perdu ? </p>
       <p><NavLink to="/">Retourner sur la page d'accueil</NavLink></p>
